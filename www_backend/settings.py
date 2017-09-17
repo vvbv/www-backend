8ca728ab285cdf8558460f8a6041ecdef509dc4f
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-09-17 03:37:10 -0500

    Fin de la prueba

--- www_backend/settings.py
+++ www_backend/settings.py
@@ -97,6 +97,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'django',
+#        'USER': 'postgres',
+#        'PASSWORD' : 'postgres',
+#        'HOST' : 'localhost',
+#        'PORT' : '',
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
