883c7bf30db3eac9977381b2061be62c21b57139
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2017-10-28 21:47:37 -0500

    Cifrado de password en update usuario.

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,3 +1,5 @@
+import crypt
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from usuarios.models import Usuario
@@ -5,7 +7,12 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 
 
+
 class UsuarioSerializer(serializers.ModelSerializer):
+
+    
+
+
     class Meta:
         model = Usuario
         fields = (
@@ -20,3 +27,12 @@ class UsuarioSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(validated_data['password'])
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
