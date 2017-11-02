87cf8cd181df5da5f758ae98af7f802e2ba2ffe2
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-11-02 14:08:14 -0500

    Corregir error de identacion

--- www_backend/settings.py
+++ www_backend/settings.py
@@ -181,6 +181,6 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'	
  		
  # Extra places for collectstatic to find static files.		
- STATICFILES_DIRS = (
+STATICFILES_DIRS = (
      os.path.join(PROJECT_ROOT, 'static'),
   )
