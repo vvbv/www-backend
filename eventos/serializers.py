b9a9a2f51138147eb81b299107e4d1425dec9adf
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-11-02 01:56:25 -0500

    Correccion de bugs en vistas y modelos evento

--- eventos/serializers.py
+++ eventos/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
 from .models import Evento, Actividad, PreInscripcionEvento
-
+from usuarios.serializers import UsuarioSerializer
 
 class EventoSerializer(serializers.ModelSerializer):
     actividades = serializers.PrimaryKeyRelatedField(many=True, read_only=True) 
+    usuariosPreinscritos = UsuarioSerializer(many=True, read_only=True)
     def validate(self, attrs):
         instance = Evento(**attrs)
         instance.clean()
         return attrs
     class Meta: 
         model = Evento
-        fields = ('id',     'nombre', 'descripcion', 'fechaInicio', 'fechaFinalizacion', 'estado', 'actividades')
+        fields = ('id',     'nombre', 'descripcion', 'fechaInicio', 'fechaFinalizacion', 'estado', 'actividades', 'usuariosPreinscritos')
     def get_estados(self, obj):
         return obj.get_estados_display()
    
