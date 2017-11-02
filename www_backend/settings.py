2635d39e92b474e87d9cc87f015bde7aeb8a294c
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-11-02 14:02:27 -0500

    Problema con stati root

--- www_backend/settings.py
+++ www_backend/settings.py
@@ -177,12 +177,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-                os.path.join(BASE_DIR,'static/'),
-)
-
-MEDIA_URL = '/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')		
+STATIC_URL = '/static/'		  STATIC_URL = '/static/'
+ 		
+ # Extra places for collectstatic to find static files.		
+ STATICFILES_DIRS = (
+     os.path.join(PROJECT_ROOT, 'static'),
+  )
